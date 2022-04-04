909c378c726497c728230c2c26f3778808234a82
enranime <enranime@gmail.com>
2022-04-04 12:18:17 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const userRouter = require('./routes/user');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
 
@@ -12,7 +14,13 @@ app.use(bodyParser.json());
 
 app.use('/users',userRouter)
 
+const boot = async () => {
+    // connect to mongodb
+    await mongoose.connect(config.mongoUri, config.mongoOptions);
+    // start express server 
+    app.listen(4000, () => {
+        console.log('Server is running');
+    });
+};
 
-app.listen(4000, () => {
-    console.log('Server is running');
-});
+boot();
--- src/routes/record.js
+++ src/routes/record.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const Joi = require('joi');
 
+
+const recordModel = require('../models/record');
+
 const createRequestSchema = Joi.object({
   activityName: Joi.string().required(),
   timestamp: Joi.string().required(),
@@ -19,67 +22,48 @@ const updateRequestSchema = Joi.object({
 
 const router = express.Router();
 
-const records = [
-  {
-    _id: 'record-1',
-    activityName: 'Running',
-    timestamp: new Date(),
-    duration: 4000,
-    calories: 200,
-    description: '',
-  },
-  {
-    _id: 'record-2',
-    activityName: 'Running',
-    timestamp: new Date(),
-    duration: 4000,
-    calories: 200,
-    description: '',
-  },
-  {
-    _id: 'record-3',
-    activityName: 'Running',
-    timestamp: new Date(),
-    duration: 4000,
-    calories: 200,
-    description: '',
-  },
-];
-
-router.use('/:recordId', (req, res, next) => {
-  const index = records.findIndex((r) => r._id === req.params.recordId);
-  const foundRecord = records[index];
+router.use('/:recordId', async (req, res, next) => {
+  const foundRecord = await RecordModel.findById(req.params.recordId);
   if (!foundRecord) {
     return res.status(404).send('Record not found');
   }
-  req.record = foundRecord;
-  req.recordIndex = index;
-  return next();
+    router.get('/:recordId', (req, res, next) => {
+    return res.send(req.record);
+    });
 });
+
 
 router.get('/:recordId', (req, res, next) => {
   return res.send(req.record);
 });
-router.get('/', (req, res, next) => {
+
+router.get('/', async(req, res, next) => {
+  const records = await RecordModel.find({});
   res.send(records);
 });
+
 router.post('/', (req, res, next) => {
   const body = req.body;
 
+  const newRecord = new RecordModel(body);
+
+  const errors = newRecord.validateSync();
+  if (errors) {
+    const errorFieldNames = Object.keys(errors.errors);
+    if (errorFieldNames.length > 0) {
+      return res.status(400).send(errors.errors[errorFieldNames[0]].message);
+    }
+  }
   // validate
   const validateResult = createRequestSchema.validate(body);
   if (validateResult.error) {
     // failed validation
     return res.status(400).send('Invalid request');
   }
-
-  const newRecord = {
-    _id: Math.floor(Math.random() * 100000).toString(),
-    ...body,
-  };
-  records.push(newRecord);
+  await newRecord.save();
   return res.status(201).send(newRecord);
 });
+
 
 router.put('/:recordId', (req, res, next) => {
   const body = req.body;
@@ -99,8 +83,13 @@ router.put('/:recordId', (req, res, next) => {
   return res.status(201).send(updatedRecord);
 });
 
-router.delete('/:recordId', (req, res, next) => {
-  records.splice(req.recordIndex, 1);
+// router.delete('/:recordId', (req, res, next) => {
+//   records.splice(req.recordIndex, 1);
+//   return res.status(204).send(); // 204 = No content which mean it successfully removed
+// });
+
+router.delete('/:recordId', async (req, res, next) => {
+  await RecordModel.deleteOne({ _id: req.params.recordId });
   return res.status(204).send(); // 204 = No content which mean it successfully removed
 });
 
