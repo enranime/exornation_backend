9ef556213adb417d9425701a35574d26d63e1958
enranime <enranime@gmail.com>
2022-04-21 09:18:13 +0700

    update

---
+++ api/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const userRouter = require('../routes/user');
+const bodyParser = require('body-parser');
+const config = require('../config')
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 4001;
+/*
+When running on Vercel, Vercel will take express "app" exported from this file.
+We do not have control over port and things that should be run before the app.listen()
+eg. mongoose.connect(). So we should connect to mongodb before every request by adding middleware below
+to ensure database is connected before our code trying to query the it.
+
+Note: Create new connection to database before every request is not a good practice and can cause issues for some database.
+ */
+if (config.isVercel) {
+  app.use(async (req, res, next) => {
+    await mongoose.connect(config.mongoUri, config.mongoOptions);
+    return next();
+  });
+}
+
+
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200,
+  }));
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+
+
+app.use('/users',userRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
+// const product = require("./api/product");
+
+// app.use(express.json({extend: false}));
+
+// app.use("/api/product", product);
+
+// app.get("/",(req,res,next) => {
+//     res.status(200).send("Hello World")
+// });
+
+// const PORT = process.env.PORT || 8080;
+
+// app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
+
+const mongoose = require('mongoose');
+const config = require('./config');
+const app = require('./api/index');
 const product = require("./api/product");
+
+
 
 app.use(express.json({extend: false}));
 
@@ -10,6 +30,17 @@ app.get("/",(req,res,next) => {
     res.status(200).send("Hello World")
 });
 
-const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+const boot = async () => {
+    // connect to mongodb
+    await mongoose.connect(config.mongoUri, config.mongoOptions
+        ).then(
+            console.log('connect to mongodb')
+        );
+    // start express server 
+    app.listen(config.port, () => {
+        console.log(`Server is running on port ${config.port}`);
+    });
+};
+
+boot();
