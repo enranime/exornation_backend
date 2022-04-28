f5b9eebfb392bd4c4f462d0e62dec63ccb1c6064
enranime <enranime@gmail.com>
2022-04-28 20:04:52 +0700

    update

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-const express = require("express");
-// const app = express();
-// const product = require("./api/product");
-
-// app.use(express.json({extend: false}));
-
-// app.use("/api/product", product);
-
-// app.get("/",(req,res,next) => {
-//     res.status(200).send("Hello World")
-// });
-
-// const PORT = process.env.PORT || 8080;
-
-// app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
-
 
 const mongoose = require('mongoose');
 const config = require('./config');
 const app = require('./api/index');
-const product = require("./api/product");
 
 
-
-app.use(express.json({extend: false}));
-
-app.use("/api/product", product);
-
-app.get("/",(req,res,next) => {
-    res.status(200).send("Hello World")
-});
 
 
 const boot = async () => {
