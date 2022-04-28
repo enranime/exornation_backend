30f0ed43efb02e5f6f9f4099cd114f69d7087392
enranime <enranime@gmail.com>
2022-04-28 20:17:42 +0700

    update path

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
-// const mongoose = require('mongoose');
-// const config = require('./config');
-// const app = require('./api/index');
+const mongoose = require('mongoose');
+const config = require('./config');
+const app = require('./api/index');
 
 
 
 
-// const boot = async () => {
-//     // connect to mongodb
-//     await mongoose.connect(config.mongoUri, config.mongoOptions
-//         ).then(
-//             console.log('connect to mongodb')
-//         );
-//     // start express server 
-//     app.listen(config.port, () => {
-//         console.log(`Server is running on port ${config.port}`);
-//     });
-// };
+const boot = async () => {
+    // connect to mongodb
+    await mongoose.connect(config.mongoUri, config.mongoOptions
+        ).then(
+            console.log('connect to mongodb')
+        );
+    // start express server 
+    app.listen(config.port, () => {
+        console.log(`Server is running on port ${config.port}`);
+    });
+};
 
-// boot();
+boot();
